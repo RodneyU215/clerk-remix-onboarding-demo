779d762b09d121c848fcc5588ac755a28423abe9
Rodney Urquhart <rodneyu85@gmail.com>
2024-03-25 21:49:37 -0700

    Adding reload to ensure user data is correct on the frontend.

--- app/routes/onboarding.tsx
+++ app/routes/onboarding.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import { ActionFunction, redirect } from '@remix-run/node';
+import React, { useEffect } from 'react';
+import { ActionFunction, json, redirect } from '@remix-run/node';
 import { getAuth } from '@clerk/remix/ssr.server';
 import { createClerkClient } from '@clerk/remix/api.server';
-import { Form, useActionData } from '@remix-run/react';
+import { Form, useActionData, useNavigate } from '@remix-run/react';
+import { useUser } from '@clerk/remix';
 
 export const action: ActionFunction = async (args) => {
   const { userId } = await getAuth(args);
@@ -11,24 +12,41 @@ export const action: ActionFunction = async (args) => {
     return redirect('/sign-in');
   }
   const params = { publicMetadata: { hasCompletedOnboarding: true } };
-
   const clerk = createClerkClient({
     secretKey: process.env.CLERK_SECRET_KEY,
   });
-  await clerk.users.updateUser(userId, params);
-  // Todo: Investigate why I'm not able to reload the user so that the metadata is updated
-  return redirect('/');
+
+  try {
+    await clerk.users.updateUser(userId, params);
+    return json({ onboardingComplete: true });
+  } catch (error) {
+    return json({ onboardingComplete: false });
+  }
 };
 
 const Onboarding: React.FC = () => {
   const actionData = useActionData<typeof action>();
+  const navigate = useNavigate();
+  const { user } = useUser();
+
+  useEffect(() => {
+    async function fetchData() {
+      if (actionData?.onboardingComplete && user) {
+        await user.reload();
+        navigate('/');
+      }
+    }
+    fetchData();
+  }, [actionData, user, navigate]);
 
   return (
     <div>
       <h1>Welcome to the Onboarding Page!</h1>
-      {actionData && actionData.error ? (
+      {actionData && !actionData.onboardingComplete ? (
         <p>
-          <span style={{ color: 'red' }}>{actionData.errors}</span>
+          <span style={{ color: 'red' }}>
+            {actionData.error || 'An unknown error occurred.'}
+          </span>
         </p>
       ) : null}
       <Form method="post">
